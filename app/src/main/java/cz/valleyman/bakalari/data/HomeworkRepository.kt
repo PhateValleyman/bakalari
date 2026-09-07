5115577388fd79e8cacc07755c34c21573a4fff1
Jonáš Nedvědický <Jonas.Ned@outlook.com>
2026-09-07 17:08:58 +0200

    Add repository layer skeleton

---
+++ app/src/main/java/cz/valleyman/bakalari/data/HomeworkRepository.kt
@@ -0,0 +1,11 @@
+package cz.valleyman.bakalari.data
+
+import cz.valleyman.bakalari.bakalari.BakalariClient
+
+class HomeworkRepository(
+    private val client: BakalariClient
+) {
+    suspend fun refreshHomework(): List<Homework> {
+        return client.getHomework()
+    }
+}
