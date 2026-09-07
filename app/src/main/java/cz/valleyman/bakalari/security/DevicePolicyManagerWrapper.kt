137372b4415af9988e107d57456716c363448e3a
Jonáš Nedvědický <Jonas.Ned@outlook.com>
2026-09-07 17:15:52 +0200

    Add device policy abstraction

---
+++ app/src/main/java/cz/valleyman/bakalari/security/DevicePolicyManagerWrapper.kt
@@ -0,0 +1,11 @@
+package cz.valleyman.bakalari.security
+
+class DevicePolicyManagerWrapper {
+    fun lockDevice() {
+        // DevicePolicyManager integration will be implemented here.
+    }
+
+    fun unlockRequested() {
+        // Parent approval handling will be implemented here.
+    }
+}
