50f093e706219c6f5935aa72255154c30c6ea902
Jonáš Nedvědický <Jonas.Ned@outlook.com>
2026-09-08 01:46:17 +0000

    feat: add basic UI for mode selection

--- app/src/main/java/cz/valleyman/bakalari/MainActivity.kt
+++ app/src/main/java/cz/valleyman/bakalari/MainActivity.kt
@@ -1,10 +1,23 @@
 package cz.valleyman.bakalari
 
-import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import cz.valleyman.bakalari.child.ChildLockActivity
+import cz.valleyman.bakalari.parent.ParentActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_parent_mode).setOnClickListener {
+            startActivity(Intent(this, ParentActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btn_child_mode).setOnClickListener {
+            startActivity(Intent(this, ChildLockActivity::class.java))
+        }
     }
 }
